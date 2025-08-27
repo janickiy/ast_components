143b65bf338b40010d197f00f73b67e84b54097d
Яницкий Александр Анатольевич <a.yanitskiy@els24.com>
2025-08-27 13:00:33 +0300

    add files to project

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,10 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->string('role');
+            $table->string('login', 100)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
