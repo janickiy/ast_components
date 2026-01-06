6d759e2a6f1f9e015e3aaf9b39e335b72a3585d8
Djangoner <djangoner6@gmail.com>
2026-01-06 14:59:07 +0200

    perf: queries caching

--- app/Models/Catalog.php
+++ app/Models/Catalog.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Cache;
 
 class Catalog extends Model
 {
@@ -57,7 +58,8 @@ class Catalog extends Model
     {
         $result = self::where('id', $parent_id);
 
-        if ($result->count() > 0) {
+        if ($result->count() > 0)
+        {
             $catalog = $result->first();
             $topbar[] = [$catalog->id, $catalog->name, $catalog->slug];
 
@@ -89,23 +91,36 @@ class Catalog extends Model
 
         $catalogs = self::whereIn('id', $array_of_ids)->get();
 
-        foreach ($catalogs as $catalog) {
+        foreach ($catalogs as $catalog)
+        {
             self::removeCatalog($catalog);
         }
 
         self::removeCatalog($parent);
     }
 
+    public static function getCatalogsRaw()
+    {
+        return Cache::remember('catalogs_raw', 60 * 60, function ()
+        {
+            return self::query()->orderBy('name')->get();
+        });
+    }
+
+
     /**
      * @return int
      */
     public function getTotalProductCount(): int
     {
-        $allChildren = [$this->id];
+        return Cache::remember('catalog_total_products_' . $this->id, 60 * 60, function ()
+        {
+            $allChildren = [$this->id];
 
-        self::getAllChildren(self::query()->orderBy('name')->get(), $allChildren, $this->id);
+            self::getAllChildren(self::getCatalogsRaw(), $allChildren, $this->id);
 
-        return Products::query()->whereIn('catalog_id', $allChildren)->count();
+            return Products::query()->whereIn('catalog_id', $allChildren)->count();
+        });
     }
 
     /**
@@ -114,7 +129,8 @@ class Catalog extends Model
      */
     public function scopeRemove(): void
     {
-        foreach ($this->products as $product) {
+        foreach ($this->products as $product)
+        {
             $product->remove();
         }
 
@@ -129,7 +145,8 @@ class Catalog extends Model
     {
         $ids = [];
 
-        foreach ($category->children ?? [] as $row) {
+        foreach ($category->children ?? [] as $row)
+        {
             $ids[] = $row->id;
             $ids = array_merge($ids, self::getChildren($row));
         }
@@ -165,7 +182,8 @@ class Catalog extends Model
         $lvl++;
         $rows = self::where('parent_id', $parent_id)->orderBy('name')->get();
 
-        foreach ($rows as $row) {
+        foreach ($rows as $row)
+        {
             $indent = '';
             for ($i = 1; $i < $lvl; $i++) $indent .= '-';
 
@@ -187,15 +205,20 @@ class Catalog extends Model
     {
         $cl = '';
 
-        if (isset($catalogs[$parent_id])) {
+        if (isset($catalogs[$parent_id]))
+        {
             $cl .= '<ul>';
-            if ($only_parent === false) {
-                foreach ($catalogs[$parent_id] as $catalog) {
+            if ($only_parent === false)
+            {
+                foreach ($catalogs[$parent_id] as $catalog)
+                {
                     $cl .= '<li>' . $catalog['name'] . ' <a title="Добавить подкатегорию" href="' . URL::route('admin.catalog.create', ['parent_id' => $catalog['id']]) . '"> <span class="fa fa-plus"></span> </a> <a title="Редактировать" href="' . URL::route('admin.catalog.edit', ['id' => $catalog['id']]) . '"> <span class="fa fa-edit"></span> </a> <a title="Удалить" href="' . URL::route('admin.catalog.destroy', ['id' => $catalog['id']]) . '"> <span class="fa fa-trash"></span> </a>';
                     $cl .= self::buildTree($catalogs, $catalog['id']);
                     $cl .= '</li>';
                 }
-            } else {
+            }
+            else
+            {
                 $catalog = $catalogs[$parent_id][$only_parent];
                 $cl .= '<li>' . $catalog['name'] . ' #' . $catalog['id'];
                 $cl .= self::buildTree($catalogs, $catalog['id'], true);
@@ -215,11 +238,13 @@ class Catalog extends Model
      */
     public static function getAllChildren(object $categories, array &$allChildren, int $parent_id = 0): void
     {
-        $cats = $categories->filter(function ($item) use ($parent_id) {
+        $cats = $categories->filter(function ($item) use ($parent_id)
+        {
             return $item->parent_id == $parent_id;
         });
 
-        foreach ($cats as $cat) {
+        foreach ($cats as $cat)
+        {
             array_push($allChildren, $cat->id);
             self::getAllChildren($categories, $allChildren, $cat->id);
         }
@@ -230,10 +255,11 @@ class Catalog extends Model
      */
     public static function getCatalogList(): array
     {
-        $catalogs = self::query()->orderBy('name')->get();
+        $catalogs = self::getCatalogsRaw();
         $catalogsList = [];
 
-        foreach ($catalogs->toArray() ?? [] as $catalog) {
+        foreach ($catalogs->toArray() ?? [] as $catalog)
+        {
             $catalogsList[$catalog['parent_id']][$catalog['id']] = $catalog;
         }
 
